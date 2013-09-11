03a3105f50e0c1626c474ab8c011b1ddfa2c189f
Kapps <opantm2+git@gmail.com>
2013-09-10 20:44:32 -0600

    Re-fetch test.

---
+++ src/ShardTools/Enforce.d
@@ -0,0 +1,18 @@
+﻿/// Provides basic helpers for enforcing data.
+module ShardTools.Enforce;
+import ShardTools.ExceptionTools;
+import std.conv;
+mixin(MakeException("AssertEqualFailedException"));
+mixin(MakeException("AssertNotEqualFailedException"));
+
+/// Enforces that the two values are equal to one another.
+void EnforceEqual(T, T2)(in T first, in T2 second, string FileName = __FILE__, int LineNum = __LINE__) {
+	if(first != second)
+		throw new AssertEqualFailedException("Expected " ~ to!string(first) ~ " to equal " ~ to!string(second) ~ " in " ~ FileName ~ "(" ~ to!string(LineNum) ~ ".");
+}
+
+/// Enforces that the two values are not equal to one another.
+void EnforceUnequal(T, T2)(in T first, in T2 second, string FileName = __FILE__, int LineNum = __LINE__) {
+	if(first == second)
+		throw new AssertNotEqualFailedException("Expected " ~ to!string(first) ~ " to equal " ~ to!string(second) ~ " in " ~ FileName ~ "(" ~ to!string(LineNum) ~ ".");
+}
